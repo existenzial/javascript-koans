77f121082fd87afcd9895015d8b289d4d74eb8cf
Cho S. Kim <4cho.kim@gmail.com>
2014-05-22 19:39:44 -0700

    Corrects spacing issues

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -1,7 +1,6 @@
-var _; //globals
+var _; // globals
 
 describe("About Applying What We Have Learnt", function() {
-
   var products;
 
   beforeEach(function () { 
@@ -17,7 +16,6 @@ describe("About Applying What We Have Learnt", function() {
   /*********************************************************************************/
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (imperative)", function () {
-
     var i,j,hasMushrooms, productsICanEat = [];
 
     for (i = 0; i < products.length; i+=1) {
@@ -36,7 +34,6 @@ describe("About Applying What We Have Learnt", function() {
   });
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
-
       var productsICanEat = [];
 
       /* solve using filter() & all() / any() */
@@ -47,8 +44,8 @@ describe("About Applying What We Have Learnt", function() {
   /*********************************************************************************/
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (imperative)", function () {
-    
     var sum = 0;
+
     for(var i=1; i<1000; i+=1) {
       if (i % 3 === 0 || i % 5 === 0) {
         sum += i;
@@ -59,7 +56,6 @@ describe("About Applying What We Have Learnt", function() {
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
-
     var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
 
     expect(233168).toBe(FILL_ME_IN);
